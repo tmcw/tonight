32891f55525885de808d1d147e16e6d8a105e718
Tom MacWright <tom@macwright.org>
2014-06-08 20:22:58 -0400

    Load data.

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
+/* ex: set tabstop=2 shiftwidth=2 expandtab: */
 /** @jsx React.DOM */
 var Show = React.createClass({
   render: function() {
     /*jshint ignore:start */
+    var cx = React.addons.classSet;
+    var classes = cx({
+      allages: this.props.minage === 0,
+      show: true,
+      pad1: true
+    });
     return (
-      <div className="show pad1">
+      <div className={classes}>
         <h2 className="showTitle">
           {this.props.title}
         </h2>
+        <a href={this.props.tickets}>buy tickets</a>
       </div>
     );
     /*jshint ignore:end */
@@ -14,29 +22,45 @@ var Show = React.createClass({
 });
 
 var ShowList = React.createClass({
+  loadShowsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentWillMount: function() {
+    this.loadShowsFromServer();
+  },
   render: function() {
     /*jshint ignore:start */
-    var showNodes = this.props.data.map(function(show) {
-        return <Show title={show.title}></Show>;
+    var showNodes = this.state.data.map(function(show) {
+      return <Show
+        title={show.title}
+        minage={show.minage}
+        tickets={show.tickets}></Show>;
     });
     return (
-        <div className='shows'>
-          {showNodes}
-        </div>
+      <div className='shows'>
+        {showNodes}
+      </div>
     );
     /*jshint ignore:end */
   }
 });
 
 React.renderComponent(
-  ShowList({
-    data: [
-      {
-        title: 'Teen Mom'
-      },
-      {
-        title: 'Shark Week'
-      }
-  ]}),
+  /*jshint ignore:start */
+  <ShowList url="example.json" />,
+  /*jshint ignore:end */
   document.getElementById('content')
 );
